ffe41c595fd5edb22baf6b8aeabf5eea16840b5d
Shubham Singh <63634752+ss-sonic@users.noreply.github.com>
2026-08-25 22:47:30 +0400

    Load Lesson 06 completion layout fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import "../curriculum/03-ascii/styles.css";
 import "../curriculum/04-breaking-ascii/styles.css";
 import "../curriculum/05-unicode-code-points/styles.css";
 import "../curriculum/06-code-points-vs-bytes/styles.css";
+import "../curriculum/06-code-points-vs-bytes/completion.css";
 import "../curriculum/06a-hexadecimal/styles.css";
 import "../styles/lesson-typography.css";
 
